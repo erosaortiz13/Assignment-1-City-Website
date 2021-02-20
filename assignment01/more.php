65dca6057982f7d55b42d2b0bf04a830908fd71c
Emely Rosa <erosa4@u.rochester.edu>
2021-02-19 21:52:19 -0500

    Update more.php
    made it look like about.php in terms of divs and headings

--- assignment01/more.php
+++ assignment01/more.php
@@ -5,63 +5,45 @@
 <?php include "inc/head.inc" ?>
 
 <body>
+	<header>
+		<?php include "inc/nav.inc" ?>
+	</header>
+
+	<h2>Climate</h2>
+	<div class="container">
+
+		<div>
+		  	<img src="images/pic_three.jpg" alt="Miami Climate">	
+		</div>
 
 
-<div class="banner">
-	<a class="logo" href="index.php">
-	<h1><img src="images/logo.png" alt="logo">
-	Sunny Miami</h1></a>
-	<?php include "inc/nav.inc" ?>
-</div>
+		<div>
+		  	<p>
+		  		Miami has a tropical monsoon climate (Köppen climate classification Am) with a marked drier season in the winter. Although it is relatively cooler than most other tropical places in the winter and does get occasional 40 degree Fahrenheit (4 °C) lows, Miami nevertheless meets the minimum requirements to be in the tropical climate zone, making it one of the northernmost major cities on Earth within this classification. The city's sea-level elevation, coastal location, position just above the Tropic of Cancer, and proximity to the Gulf Stream shape its climate. Average winter high temperatures, from December to March, range from 76.4–80.3 °F (24.7–26.8 °C). January is the coolest month with an average daily temperature of 68.2 °F (20.1 °C). Low temperatures fall below 50 °F (10 °C) about 10–15 nights during the winter season,[citation needed] after the passage of cold fronts that produce much of the winter rainfall. The wet season begins sometime in June, ending in mid-October. During this period, temperatures range from the mid 80s to low 90s °F (29–35 °C) and are accompanied by high humidity, though the heat is often relieved in the afternoon by thunderstorms or a sea breeze that develops off the Atlantic Ocean. Much of the year's 61.9 inches (1,572 mm) of rainfall occurs during this period. 
+		  		
+		  </p>
 
-<div class="container">
-
-<div>
-
-	  	<h2>Climate</h2>
-
-</div>
-
-<div>
-	  	<img src="images/pic_three.jpg" alt="Miami Climate">
-	  	
-</div>
-
-
-<div>
-	  	<p>
-	  		Miami has a tropical monsoon climate (Köppen climate classification Am) with a marked drier season in the winter. Although it is relatively cooler than most other tropical places in the winter and does get occasional 40 degree Fahrenheit (4 °C) lows, Miami nevertheless meets the minimum requirements to be in the tropical climate zone, making it one of the northernmost major cities on Earth within this classification. The city's sea-level elevation, coastal location, position just above the Tropic of Cancer, and proximity to the Gulf Stream shape its climate. Average winter high temperatures, from December to March, range from 76.4–80.3 °F (24.7–26.8 °C). January is the coolest month with an average daily temperature of 68.2 °F (20.1 °C). Low temperatures fall below 50 °F (10 °C) about 10–15 nights during the winter season,[citation needed] after the passage of cold fronts that produce much of the winter rainfall. The wet season begins sometime in June, ending in mid-October. During this period, temperatures range from the mid 80s to low 90s °F (29–35 °C) and are accompanied by high humidity, though the heat is often relieved in the afternoon by thunderstorms or a sea breeze that develops off the Atlantic Ocean. Much of the year's 61.9 inches (1,572 mm) of rainfall occurs during this period. 
-	  		
-	  </p>
-
-</div>
-</div>
-<hr>
-<div class="container">
-
-	<div>
-		<h2>Geology</h2>
+		</div>
 	</div>
 
-	<div>
-	<img class="lav" src="images/pic_four.jpg" alt="Miami Geology">
-</div> 
-	<div>
+	<h2>Geology</h2>
+	<div class="container">
+		<div>
+			<img class="lav" src="images/pic_four.jpg" alt="Miami Geology">
+		</div> 
+		<div>
 		
-	  	<p> The surface bedrock under the Miami area is called Miami oolite or Miami limestone. This bedrock is covered by a thin layer of soil, and is no more than 50 feet (15 m) thick. Miami limestone formed as the result of the drastic changes in sea level associated with recent glacial periods, or ice ages. Beginning some 130,000 years ago, the Sangamonian Stage raised sea levels to approximately 25 feet (8 m) above the current level. All of southern Florida was covered by a shallow sea. Several parallel lines of reef formed along the edge of the submerged Florida plateau, stretching from the present Miami area to what is now the Dry Tortugas. The area behind this reef line was in effect a large lagoon, and the Miami limestone formed throughout the area from the deposition of oolites and the shells of bryozoans. Starting about 100,000 years ago, the Wisconsin glaciation began lowering sea levels, exposing the floor of the lagoon. By 15,000 years ago, the sea level had dropped 300 to 350 feet (90 to 110 m) below the current level. The sea level rose quickly after that, stabilizing at the current level about 4,000 years ago, leaving the mainland of South Florida just above sea level. 
-	  		
-	  	</p>
-	  
-</div>
-
-</div>
-<hr>
+		  	<p> 
+		  		The surface bedrock under the Miami area is called Miami oolite or Miami limestone. This bedrock is covered by a thin layer of soil, and is no more than 50 feet (15 m) thick. Miami limestone formed as the result of the drastic changes in sea level associated with recent glacial periods, or ice ages. Beginning some 130,000 years ago, the Sangamonian Stage raised sea levels to approximately 25 feet (8 m) above the current level. All of southern Florida was covered by a shallow sea. Several parallel lines of reef formed along the edge of the submerged Florida plateau, stretching from the present Miami area to what is now the Dry Tortugas. The area behind this reef line was in effect a large lagoon, and the Miami limestone formed throughout the area from the deposition of oolites and the shells of bryozoans. Starting about 100,000 years ago, the Wisconsin glaciation began lowering sea levels, exposing the floor of the lagoon. By 15,000 years ago, the sea level had dropped 300 to 350 feet (90 to 110 m) below the current level. The sea level rose quickly after that, stabilizing at the current level about 4,000 years ago, leaving the mainland of South Florida just above sea level. 
+		  	</p>
+		</div>
+	</div>
 
 
 
-<?php include "inc/footer.inc" ?>
 
-<?php include "inc/scripts.inc" ?>
+	<?php include "inc/footer.inc" ?>
+	<?php include "inc/scripts.inc" ?>
 
 </body>
 
