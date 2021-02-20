d09e8ad65d69336c4c3729c4861a92e996ba8786
Emely Rosa <erosa4@u.rochester.edu>
2021-02-19 22:01:19 -0500

    Update index.php
    Removed "Beaches Shopping Luxury" and "The Sunshine State"
    Move info to where read more is and removed the "..."

--- assignment01/index.php
+++ assignment01/index.php
@@ -5,25 +5,18 @@
 <?php include "inc/head.inc" ?>
 
 <body class="homebody">
-<div class="home">
-<header>
-	<a href="index.php">
-    <img src="images/logo.png" alt="logo">
-	</a>
-	 
-	 <a class="wiki" href="https://en.wikipedia.org/wiki/miami">Information Source...</a>
-</header>
+	<div class="home">
+		<header>
+			<a href="index.php">
+		    <img src="images/logo.png" alt="logo">
+			</a>
+		</header>
 
-<div class="brief">
-	  Beaches Shopping Luxury
-	  <a class ="content" href="about.php">Read More...</a>
-</div>
-
-<div class="celer">
-	The Sunshine State
-</div>
-
-</div>
+		<div class="brief">
+			  <a class ="content" href="about.php">Read More</a>
+			  <a class="wiki" href="https://en.wikipedia.org/wiki/miami">Information Source</a>
+		</div>
+	</div>
 
 <?php include "inc/scripts.inc" ?>
 
