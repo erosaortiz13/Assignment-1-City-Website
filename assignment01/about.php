c4d70f9404feeb806bf856952b4cce2164e14cbb
Emely Rosa <erosa4@u.rochester.edu>
2021-02-19 21:43:59 -0500

    Improved Indentation

--- assignment01/about.php
+++ assignment01/about.php
@@ -8,42 +8,42 @@
 	<header>
 		<?php include "inc/nav.inc" ?>
 	</header>
+
 	<h2>Miami Beach</h2>
+	<div class="container">
+		<div>
+			<img src="images/pic_one.jpg" alt="Miami Beach">
+		</div>
 
-<div class="container">
-	<div>
-		<img src="images/pic_one.jpg" alt="Miami Beach">
+
+		<div>
+		  	<p>
+		  		Miami Beach is a coastal resort city in Miami-Dade County, Florida, United States. It was incorporated on March 26, 1915. The municipality is located on natural and man-made barrier islands between the Atlantic Ocean and Biscayne Bay, the latter of which separates the Beach from the mainland city of Miami. The neighborhood of South Beach, comprising the southernmost 2.5 square miles (6.5 km2) of Miami Beach, along with downtown Miami and the Port of Miami, collectively form the commercial center of South Florida. Miami Beach's estimated population is 88,885 according to the most recent United States census estimates. Miami Beach is the 26th largest city in Florida based on official 2019 estimates from the US Census Bureau. It has been one of America's pre-eminent beach resorts since the early 20th century. Florida as a whole is a very tropical state. 
+		  		
+		  </p>
+		</div>
+
+	</div>
+
+	<h2>Key Locales</h2>
+	<div class="container">
+		<div>
+			<img class="lav" src="images/pic_two.jpg" alt="Miami Nightlife">
+		</div> 
+		<div>
+			
+		  	<p> The southern side of Miami includes the neighborhoods of Coral Way, The Roads and Coconut Grove. Coral Way is a historic residential neighborhood built in 1922 between Downtown and Coral Gables, and is home to many old homes and tree-lined streets. Coconut Grove, established in 1825, is a historic neighborhood with narrow, winding roads, and a heavy tree canopy. It is the location of Miami's City Hall at Dinner Key, the former Coconut Grove Playhouse, CocoWalk, and the Coconut Grove Convention Center. It is also home to many nightclubs, bars, restaurants, and bohemian shops, which makes it very popular with local college students. Coconut Grove is known for its many parks and gardens, such as Vizcaya Museum, The Kampong, The Barnacle Historic State Park, and numerous other historic homes and estates. 
+		  		
+		  	</p>
+		  
+		</div>
+
 	</div>
 
 
-	<div>
-	  	<p>
-	  		Miami Beach is a coastal resort city in Miami-Dade County, Florida, United States. It was incorporated on March 26, 1915. The municipality is located on natural and man-made barrier islands between the Atlantic Ocean and Biscayne Bay, the latter of which separates the Beach from the mainland city of Miami. The neighborhood of South Beach, comprising the southernmost 2.5 square miles (6.5 km2) of Miami Beach, along with downtown Miami and the Port of Miami, collectively form the commercial center of South Florida. Miami Beach's estimated population is 88,885 according to the most recent United States census estimates. Miami Beach is the 26th largest city in Florida based on official 2019 estimates from the US Census Bureau. It has been one of America's pre-eminent beach resorts since the early 20th century. Florida as a whole is a very tropical state. 
-	  		
-	  </p>
-	</div>
+	<?php include "inc/footer.inc" ?>
 
-</div>
-
-<h2>Key Locales</h2>
-<div class="container">
-	<div>
-		<img class="lav" src="images/pic_two.jpg" alt="Miami Nightlife">
-	</div> 
-	<div>
-		
-	  	<p> The southern side of Miami includes the neighborhoods of Coral Way, The Roads and Coconut Grove. Coral Way is a historic residential neighborhood built in 1922 between Downtown and Coral Gables, and is home to many old homes and tree-lined streets. Coconut Grove, established in 1825, is a historic neighborhood with narrow, winding roads, and a heavy tree canopy. It is the location of Miami's City Hall at Dinner Key, the former Coconut Grove Playhouse, CocoWalk, and the Coconut Grove Convention Center. It is also home to many nightclubs, bars, restaurants, and bohemian shops, which makes it very popular with local college students. Coconut Grove is known for its many parks and gardens, such as Vizcaya Museum, The Kampong, The Barnacle Historic State Park, and numerous other historic homes and estates. 
-	  		
-	  	</p>
-	  
-	</div>
-
-</div>
-
-
-<?php include "inc/footer.inc" ?>
-
-<?php include "inc/scripts.inc" ?>
+	<?php include "inc/scripts.inc" ?>
 
 </body>
 
